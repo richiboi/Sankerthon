547dabe7984c61f20d49df8546cdbfd1a098cafc
Richard Cheung <richard.yh.cheung@gmail.com>
2020-06-15 16:10:48 +0800

    Started leaderboard support

--- src/components/ChallengeScreen.js
+++ src/components/ChallengeScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./../ChallengeScreen.css";
+import Leaderboard from "./leaderboard/Leaderboard";
 
 export default function ChallengeScreen() {
   return (
-    <div className="challenge-background">
+    <div className="challenge-wrapper">
       <div className="challenge-container">
         <h4 id="above-sankerthon">Welcome to the</h4>
         <h1 id="sankerthon">Sankerthon</h1>
@@ -26,6 +27,7 @@ export default function ChallengeScreen() {
           </li>
         </ul>
       </div>
+      <Leaderboard />
     </div>
   );
 }
